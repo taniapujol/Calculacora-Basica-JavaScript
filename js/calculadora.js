101e6b0c1ea06ccf2940273d433bffd56684a529
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-03-18 10:28:11 +0100

    obter el sass anterior

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,32 +2,8 @@
 $(document).ready(function(){
   // declaracion de variables globales
   var testeo = true;
-  if (testeo) console.log ("jquery cargado");
-  // Abrir ventana calculadora
-  $("#abir").click(function(){
-    $(".ventana").slideDown();
+  $.getScript('js/funciones.js', function(){
+    if (testeo) console.log ("funciones cargadas");
   });
-  // cerrar ventana calculadora
-  $("#cerrar").click(function(){
-    $(".ventana").slideUp();
-  });
-  // Funcion obtener numero (Boton (0)..(9))
-
-  // Funcion suma (Boton (+))
-
-  // Funcion resta (Boton (-))
-
-  // Funcion multiplicar (Boton (*))
-
-  // Funcion dividir (Boton (/))
-
-  // Funcion Borrar (Boton (C))
-
-  // Funcion Borrar un numero (Boton (CE))
-
-  // Funcion punto de decimal (Boton (.))
-
-  // Funcion resultado (Boton (=))
-
   // Fin js
 });
---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+// Abrir ventana calculadora
+$("#abir").click(function(){
+  $(".ventana").slideDown();
+});
+
+// cerrar ventana calculadora
+$("#cerrar").click(function(){
+  $(".ventana").slideUp();
+});
+
+// Funcion obtener numero (Boton (0)..(9))
+
+// Funcion suma (Boton (+))
+
+// Funcion resta (Boton (-))
+
+// Funcion multiplicar (Boton (*))
+
+// Funcion dividir (Boton (/))
+
+// Funcion Borrar (Boton (C))
+
+// Funcion Borrar un numero (Boton (CE))
+
+// Funcion punto de decimal (Boton (.))
+
+// Funcion resultado (Boton (=))
