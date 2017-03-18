e7e4d47c71f8674d0882810f9705d6f35e354485
SURFACE\ainat <ainatpujolrigual@gmail.com>
2017-03-18 11:49:32 +0100

    optener commit anterior

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,8 +2,35 @@
 $(document).ready(function(){
   // declaracion de variables globales
   var testeo = true;
-  $.getScript('js/funciones.js', function(){
-    if (testeo) console.log ("funciones cargadas");
+  if (testeo) console.log("cargado Jquery");
+  // Abrir ventana calculadora
+  $("#abir").click(function(){
+    $(".ventana").slideDown();
   });
+
+  // cerrar ventana calculadora
+  $("#cerrar").click(function(){
+    $(".ventana").slideUp();
+  });
+
+  // Funcion obtener numero (Boton (0)..(9))
+
+  // Funcion suma (Boton (+))
+
+  // Funcion resta (Boton (-))
+
+  // Funcion multiplicar (Boton (*))
+
+  // Funcion dividir (Boton (/))
+
+  // Funcion Borrar (Boton (C))
+
+  // Funcion Borrar un numero (Boton (CE))
+
+  // Funcion punto de decimal (Boton (.))
+
+  // Funcion resultado (Boton (=))
+
+
   // Fin js
 });
